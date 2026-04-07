c5058db280a4c08a4e22a772e8ad4844cdafceec
Jonathan Kaleve <jonathan.kaleve@gmail.com>
2026-04-07 16:49:36 +0200

    Target JDK 21 (matches F-Droid build server and Android Studio bundled JDK)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     buildFeatures { compose = true }
 }
@@ -47,5 +47,5 @@ dependencies {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
