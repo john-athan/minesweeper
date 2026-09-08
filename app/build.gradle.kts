507124b47696871105e612070105ced130b9c490
Jonathan Kaleve <jonathan.kaleve@gmail.com>
2026-09-08 07:43:53 +0200

    release: 1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId  = "io.github.johnathan.minesweeper"
         minSdk         = 21
         targetSdk      = 35
-        versionCode    = 4
-        versionName    = "1.3"
+        versionCode    = 5
+        versionName    = "1.4"
     }
 
     buildTypes {
